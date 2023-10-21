10a1b28378b1dfd3236f4871c3b2d0641c49c6ff
hsedr <haaseduard@web.de>
2023-10-21 14:20:52 +0200

    added widget weather

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <script>
-    import GarbageWidget from "$lib/widgets/GarbageWidget.svelte";
+  import GarbageWidget from "$lib/widgets/GarbageWidget.svelte";
+  import Roadworks from "$lib/widgets/RoadworksWidget.svelte";
+  import WeatherWidget from "$lib/widgets/WeatherWidget.svelte";
 </script>
 
 <div class="prose">
-    <h1>Welcome to SvelteKit</h1>
-    <p>
-        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-    </p>
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</div>
+<div class="mockup-phone border-primary">
+  <div class="camera" />
+  <div class="display">
+    <div class="artboard phone-1">
+      <WeatherWidget/>
+    </div>
+  </div>
 </div>
