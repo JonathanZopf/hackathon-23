c4f949036df35750633b49f04e0005cf95fe5bbc
Zylence <kirsch.julian@yahoo.com>
2023-10-21 14:28:39 +0200

    Add IAQ Widget

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import GarbageWidget from "$lib/widgets/GarbageWidget.svelte";
+    import IAQWidget from "$lib/widgets/IAQWidget.svelte";
 </script>
 
 <div class="prose">
@@ -7,4 +8,8 @@
     <p>
         Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
     </p>
+</div>
+<div class="artboard phone-1">
+    <IAQWidget></IAQWidget>
+
 </div>
